7ac654c6f1df4eece262e7e8faa20fdfcf89a6a5
ooblog <ooblog@gmail.com>
2016-09-08 01:42:18 +0900

    「LTsv/kanglyph.tsv」「LTsv/LTsv_gui.py」「LTsv/LTsv_kbd.py」などからLTsv9kantray用の旧式漢直キーボードの関数などを除去。

--- LTsv/LTsv_kbd.py
+++ LTsv/LTsv_kbd.py
@@ -53,25 +53,6 @@ LTsv_BTNnameL=["330","331","332","277","278"]
 LTsv_BTNcodeL=[ 250,  251,  252,  253 , 254 ]
 LTsv_BTNdic=dict(zip(LTsv_BTNnameL,LTsv_BTNcodeL))
 
-LTsv_keyboard_iroha=  ["ぬ",     "ふ",     "あ",     "う",     "え",     "お",     "や",     "ゆ",     "よ",     "わ",     "ほ",     "へ",     "た",     "て",     "い",     "す"     ,"か",     "ん",     "な",     "に",     "ら",     "せ",     "゛",     "゜",     "ち",     "と",     "し",     "は",     "き",     "く",     "ま",     "の",     "り",     "れ",     "け",     "む",     "つ",     "さ",     "そ",     "ひ",     "こ",     "み",      "も",      "ね",      "る",      "め",     "ろ",     "￥",     "　"]
-LTsv_keyboard_guideN= {"ぬ":"ﾇ", "ふ":"ﾌ", "あ":"ｱ", "う":"ｳ", "え":"ｴ", "お":"ｵ", "や":"ﾔ", "ゆ":"ﾕ", "よ":"ﾖ", "わ":"ﾜ", "ほ":"ﾎ", "へ":"ﾍﾎ","た":"ﾀ", "て":"ﾃ", "い":"ｲ", "す":"ｽ", "か":"ｶ", "ん":"ｶ", "な":"ﾅ", "に":"ﾅﾆ","ら":"ﾗ", "せ":"ｾ", "゛":"＠","゜":"ぷ","ち":"ﾁ", "と":"ﾄ", "し":"ｼ", "は":"ﾊ", "き":"ｷ", "く":"ｸ", "ま":"ﾏ", "の":"ﾉ", "り":"ﾘ", "れ":"ﾚ", "け":"ｹ", "む":"ﾑ", "つ":"ﾂ", "さ":"ｻ", "そ":"ｿ", "ひ":"ﾋ", "こ":"ｺ", "み":"ﾐ",  "も":"ﾓ",  "ね":"ﾈ",  "る":"ﾙ",  "め":"ﾒﾙ","ろ":"ﾛ", "￥":"\\","α":"α","β":"β","γ":"γ","δ":"δ","ε":"ε","ζ":"ζ","η":"η","θ":"θ","ι":"ι","κ":"κ","λ":"λ","μ":"μ","ν":"ν","ξ":"ξ","ο":"ο","π":"π","ρ":"ρ","σ":"σ","τ":"τ","υ":"υ","φ":"φ","χ":"χ","ψ":"ψ","ω":"ω","○":"○","△":"△","□":"□"}
-LTsv_keyboard_guideX= {"ぬ":"ﾇ", "ふ":"ﾌ", "あ":"ｱ", "う":"ｳ", "え":"ｱ", "お":"ｵ", "や":"ｼｬ","ゆ":"ｼｭ","よ":"ｼｮ","わ":"ｼｮ","ほ":"ﾎ", "へ":"ﾀ", "た":"ﾀ", "て":"ﾀﾃ","い":"ｲ", "す":"ｽ", "か":"ｶ", "ん":"ｶ", "な":"ｶ", "に":"ﾆﾄ","ら":"ｼ", "せ":"ｾ", "゛":"｀","゜":"プ","ち":"ﾁ", "と":"ﾄ", "し":"ｼ", "は":"ﾊ", "き":"ｷ", "く":"ｸ", "ま":"ﾏ", "の":"ﾊ", "り":"ﾊ", "れ":"ｷ", "け":"ｹ", "む":"ﾑ", "つ":"ﾂ", "さ":"ｻ", "そ":"ｿ", "ひ":"ﾋ", "こ":"ｺ", "み":"ｻﾐ", "も":"ｿﾓ", "ね":"ﾋﾈ", "る":"ﾙｺ", "め":"ﾒ", "ろ":"ﾛ", "￥":"|" ,"α":"Α","β":"Β","γ":"Γ","δ":"Δ","ε":"Ε","ζ":"Ζ","η":"Η","θ":"Θ","ι":"Ι","κ":"Κ","λ":"Λ","μ":"Μ","ν":"Ν","ξ":"Ξ","ο":"Ο","π":"Π","ρ":"Ρ","σ":"Σ","τ":"Τ","υ":"Υ","φ":"Φ","χ":"Χ","ψ":"Ψ","ω":"Ω","○":"●","△":"▲","□":"■"}
-LTsv_keyboard_guideK= ["名",     "音",     "訓",     "送",     "異",     "俗",     "簡",     "繁",     "越",     "地",     "逆",     "非",     "英",     "顔",     "ε",     "ρ",     "τ",     "υ",     "θ",     "ι",     "ο",     "π",     "゛",     "゜",     "α",     "σ",     "δ",     "φ",     "γ",     "η",     "ξ",     "κ",     "λ",     "代",     "鍵",     "ぬ",     "ζ",     "χ",     "ψ",     "ω",     "β",     "ν",      "μ",      "熙",      "○",      "△",     "□",     "￥",     "σ"]
-LTsv_keyboard_guideKN={"ぬ":"名","ふ":"音","あ":"訓","う":"送","え":"異","お":"俗","や":"簡","ゆ":"繁","よ":"越","わ":"地","ほ":"逆","へ":"非","た":"英","て":"顔","い":"ε","す":"ρ","か":"τ","ん":"υ","な":"θ","に":"ι","ら":"ο","せ":"π","゛":"＠","゜":"ぷ","ち":"α","と":"σ","し":"δ","は":"φ","き":"γ","く":"η","ま":"ξ","の":"κ","り":"λ","れ":"代","け":"鍵","む":"ぬ","つ":"ζ","さ":"χ","そ":"ψ","ひ":"ω","こ":"β","み":"ν", "も":"μ", "ね":"熙", "る":"○", "め":"△","ろ":"□","￥":"\\","　":"σ"}
-LTsv_keyboard_guideKX={"ぬ":"名","ふ":"音","あ":"訓","う":"送","え":"異","お":"俗","や":"簡","ゆ":"繁","よ":"越","わ":"地","ほ":"逆","へ":"非","た":"英","て":"顔","い":"Ε","す":"Ρ","か":"Τ","ん":"Υ","な":"Θ","に":"Ι","ら":"Ο","せ":"Π","゛":"｀","゜":"プ","ち":"Α","と":"Σ","し":"Δ","は":"Φ","き":"Γ","く":"Η","ま":"Ξ","の":"Κ","り":"Λ","れ":"代","け":"鍵","む":"ヌ","つ":"Ζ","さ":"Χ","そ":"Ψ","ひ":"Ω","こ":"Β","み":"Ν", "も":"Μ", "ね":"熙", "る":"○", "め":"△","ろ":"□","￥":"\\","　":"σ"}
-LTsv_keyboard_findN=  {"ぬ":"ぬ","ふ":"ふ","あ":"あ","う":"う","え":"え","お":"お","や":"や","ゆ":"ゆ","よ":"よ","わ":"わ","ほ":"ほ","へ":"へ","た":"た","て":"て","い":"い","す":"す","か":"か","ん":"ん","な":"な","に":"に","ら":"ら","せ":"せ","゛":"゛","゜":"゜","ち":"ち","と":"と","し":"し","は":"は","き":"き","く":"く","ま":"ま","の":"の","り":"り","れ":"れ","け":"け","む":"む","つ":"つ","さ":"さ","そ":"そ","ひ":"ひ","こ":"こ","み":"み","も":"も","ね":"ね","る":"る","め":"め","ろ":"ろ","￥":"￥",    "α":"α","β":"β","γ":"γ","δ":"δ","ε":"ε","ζ":"ζ","η":"η","θ":"θ","ι":"ι","κ":"κ","λ":"λ","μ":"μ","ν":"ν","ξ":"ξ","ο":"ο","π":"π","ρ":"ρ","σ":"σ","τ":"τ","υ":"υ","φ":"φ","χ":"χ","ψ":"ψ","ω":"ω","○":"○","△":"△","□":"□"}
-LTsv_keyboard_findX=  {"ヌ":"ぬ","フ":"ふ","ア":"あ","ウ":"う","エ":"え","オ":"お","ヤ":"や","ユ":"ゆ","ヨ":"よ","ワ":"わ","ホ":"ほ","ヘ":"へ","タ":"た","テ":"て","イ":"い","ス":"す","カ":"か","ン":"ん","ナ":"な","ニ":"に","ラ":"ら","セ":"せ","｀":"゛","｛":"゜","チ":"ち","ト":"と","シ":"し","ハ":"は","キ":"き","ク":"く","マ":"ま","ノ":"の","リ":"り","レ":"れ","ケ":"け","ム":"む","ツ":"つ","サ":"さ","ソ":"そ","ヒ":"ひ","コ":"こ","ミ":"み","モ":"も","ネ":"ね","ル":"る","メ":"め","ロ":"ろ","｜":"￥",    "Α":"α","Β":"β","Γ":"γ","Δ":"δ","Ε":"ε","Ζ":"ζ","Η":"η","Θ":"θ","Ι":"ι","Κ":"κ","Λ":"λ","Μ":"μ","Ν":"ν","Ξ":"ξ","Ο":"ο","Π":"π","Ρ":"ρ","Σ":"σ","Τ":"τ","Υ":"υ","Φ":"φ","Χ":"χ","Ψ":"ψ","Ω":"ω","●":"○","▲":"△","■":"□"}
-LTsv_keyboard_findNr,LTsv_keyboard_findXr={val:key for key,val in LTsv_keyboard_findN.items()},{val:key for key,val in LTsv_keyboard_findX.items()}
-LTsv_keyboard_dicinput= [" "]*len(LTsv_keyboard_iroha)
-LTsv_keyboard_irohatype=["ぬ","ふ","あ","う","え","お","や","ゆ","よ","わ","ほ","へ","た","て","い","す","か","ん","な","に","ら","せ","゛","゜","ち","と","し","は","き","く","ま","の","り","れ","け","む","つ","さ","そ","ひ","こ","み","も","ね","る","め","ろ","￥"]
-LTsv_keyboard_alphatype=["α","β","γ","δ","ε","ζ","η","θ","ι","κ","λ","μ","ν","ξ","ο","π","ρ","σ","τ","υ","φ","χ","ψ","ω","○","△","□"]
-LTsv_keyboard_dictype=  ["英","名","音","訓","送","異","俗","熙","簡","繁","越","地","顔","鍵","代","逆","非","難","活","幅"]
-LTsv_keyboard_tofu='✖'
-LTsv_keyboard_irohamax,LTsv_keyboard_alphapos,LTsv_keyboard_guidepos,LTsv_keyboard_dicinppos,LTsv_keyboard_dicselpos=48,49,50,51,52
-LTsv_keyboard_cursorMS,LTsv_keyboard_cursorIR,LTsv_keyboard_cursorAF,LTsv_keyboard_cursorOLD,LTsv_keyboard_cursorDIC,LTsv_keyboard_cursorNX,LTsv_keyboard_cursorK,LTsv_keyboard_cursorLCR=LTsv_keyboard_alphapos,0,0,LTsv_keyboard_alphapos,LTsv_keyboard_dictype[0],True,0,"000"
-LTsv_keyboard_kanmapN,LTsv_keyboard_kanmapX,LTsv_keyboard_kandic={},{},""
-
-
 LTsv_grabflagdef=0
 LTsv_EVIOCGRAB        =0x40044590
 LTsv_defkbddevpath    ="/dev/input/event3"; LTsv_kbddevpath=LTsv_defkbddevpath
@@ -122,9 +103,6 @@ def LTsv_kbdreset(LTsv_tsvpath):
     global LTsv_typekanaW,LTsv_typekanaL,LTsv_typecodeW,LTsv_typecodeL
     global LTsv_typenameW,LTsv_typenameL,LTsv_typegana
     global LTsv_BTNnameL,LTsv_BTNcodeL,LTsv_BTNdic
-    global LTsv_keyboard_iroha,LTsv_keyboard_guideN,LTsv_keyboard_guideX,LTsv_keyboard_guideK,LTsv_keyboard_guideKN,LTsv_keyboard_guideKX
-    global LTsv_keyboard_kanmapN,LTsv_keyboard_kanmapX,LTsv_keyboard_tofu,LTsv_keyboard_kandic,LTsv_keyboard_irohatype,LTsv_keyboard_alphatype,LTsv_keyboard_dictype
-    global LTsv_keyboard_findN,LTsv_keyboard_findX,LTsv_keyboard_findNr,LTsv_keyboard_findXr
 
     LTsv_tsvdir=os.path.normpath(os.path.dirname(LTsv_tsvpath))+"/"
     LTsv_kbdltsv=LTsv_loadfile(LTsv_tsvpath)
@@ -178,51 +156,6 @@ def LTsv_kbdreset(LTsv_tsvpath):
     LTsv_keydefault_page=LTsv_getpage(LTsv_kbdltsv,"LTsv_keydefault")
     if len(LTsv_keydefault_page) > 0:
         LTsv_defkbdformat=LTsv_keydefault_page
-    keyboard_iroha_page=LTsv_getpage(LTsv_kbdltsv,"keyboard_iroha")
-    if len(keyboard_iroha_page) > 0:
-        LTsv_keyboard_iroha=LTsv_joindatanum(keyboard_iroha_page,0,"").strip('\n').split('\t')
-    keyboard_guideN_page=LTsv_getpage(LTsv_kbdltsv,"keyboard_guideN")
-    if len(keyboard_guideN_page) > 0:
-        LTsv_keyboard_guideN=LTsv_label2dictstr(keyboard_guideN_page)
-    keyboard_guideX_page=LTsv_getpage(LTsv_kbdltsv,"keyboard_guideX")
-    if len(keyboard_guideX_page) > 0:
-        LTsv_keyboard_guideX=LTsv_label2dictstr(keyboard_guideX_page)
-    keyboard_guideK_page=LTsv_getpage(LTsv_kbdltsv,"keyboard_guideK")
-    if len(keyboard_guideK_page) > 0:
-        LTsv_keyboard_guideK=LTsv_joindatanum(keyboard_guideK_page,0,"").strip('\n').split('\t')
-    keyboard_guideKN_page=LTsv_getpage(LTsv_kbdltsv,"keyboard_guideKN")
-    if len(keyboard_guideKN_page) > 0:
-        LTsv_keyboard_guideKN=LTsv_label2dictstr(keyboard_guideKN_page)
-    keyboard_guideKX_page=LTsv_getpage(LTsv_kbdltsv,"keyboard_guideKX")
-    if len(keyboard_guideKX_page) > 0:
-        LTsv_keyboard_guideKX=LTsv_label2dictstr(keyboard_guideKX_page)
-    keyboard_findN_page=LTsv_getpage(LTsv_kbdltsv,"keyboard_findN")
-    if len(keyboard_findN_page) > 0:
-       LTsv_keyboard_findN=LTsv_label2dictstr(keyboard_findN_page)
-       LTsv_keyboard_findNr={val:key for key,val in LTsv_keyboard_findN.items()}
-    keyboard_findX_page=LTsv_getpage(LTsv_kbdltsv,"keyboard_findX")
-    if len(keyboard_findX_page) > 0:
-       LTsv_keyboard_findX=LTsv_label2dictstr(keyboard_findX_page)
-       LTsv_keyboard_findXr={val:key for key,val in LTsv_keyboard_findX.items()}
-    keyboard_mapdic_page=LTsv_getpage(LTsv_kbdltsv,"keyboard_mapdic")
-    if len(keyboard_mapdic_page) > 0:
-        tofuT=LTsv_readlinerest(keyboard_mapdic_page,"tofu")
-        LTsv_keyboard_tofu='✖' if len(tofuT) == 0 else tofuT[0] if len(tofuT) >= 2 else tofuT
-        keyboard_alphatypeT=LTsv_readlinerest(keyboard_mapdic_page,"alphatype")
-        LTsv_keyboard_alphatype=LTsv_joindatanum(keyboard_alphatypeT,0,"").strip('\n').split('\t') if len(keyboard_alphatypeT) > 0 else keyboard_alphatype
-        keyboard_dictypeT=LTsv_readlinerest(keyboard_mapdic_page,"dictype")
-        LTsv_keyboard_dictype=LTsv_joindatanum(keyboard_dictypeT,0,"").strip('\n').split('\t') if len(keyboard_dictypeT) > 0 else keyboard_dictype
-        keyboard_kanmapT=LTsv_loadfile(os.path.normpath(LTsv_tsvdir+LTsv_readlinerest(keyboard_mapdic_page,"mapname")))
-        LTsv_keyboard_guide=LTsv_keyboard_guideN.keys()
-        for keyboard_guide in LTsv_keyboard_guide:
-            keyboard_lineT=LTsv_readlinerest(keyboard_kanmapT,keyboard_guide)
-            keyboard_line=keyboard_lineT.split('\t'); keyboard_line=keyboard_line+[" "]*(LTsv_keyboard_alphapos*2-len(keyboard_line))
-            keyboard_line=[(char if len(char) else " ") for char in keyboard_line]
-            LTsv_keyboard_kanmapN[keyboard_guide]=keyboard_line[:LTsv_keyboard_alphapos]
-            LTsv_keyboard_kanmapX[keyboard_guide]=keyboard_line[LTsv_keyboard_alphapos:LTsv_keyboard_alphapos*2]
-        LTsv_keyboard_kanmapN["　"],LTsv_keyboard_kanmapX["　"]=LTsv_keyboard_kanmapN[LTsv_keyboard_alphatype[0]],LTsv_keyboard_kanmapX[LTsv_keyboard_alphatype[0]]
-        LTsv_keyboard_guideN["　"],LTsv_keyboard_guideX["　"]=LTsv_keyboard_guideN[LTsv_keyboard_alphatype[0]],LTsv_keyboard_guideX[LTsv_keyboard_alphatype[0]]
-        LTsv_keyboard_kandic=LTsv_loadfile(os.path.normpath(LTsv_tsvdir+LTsv_readlinerest(keyboard_mapdic_page,"dicname")))
 
 def LTsv_kbdgettypename(LTsv_code):
     LTsv_name=""
@@ -417,40 +350,6 @@ def LTsv_getkbdkanas():
             if LTsv_kbdkeep[LTsv_typenameL[LTsv_value]]:
                 LTsv_kanas+=LTsv_key+'\t'
     return LTsv_kanas.rstrip('\t')
-
-def LTsv_keyboard_iroha_guide():
-    return LTsv_keyboard_irohamax,LTsv_keyboard_alphapos,LTsv_keyboard_guidepos,LTsv_keyboard_dicinppos,LTsv_keyboard_dicselpos,LTsv_keyboard_iroha,LTsv_keyboard_guideN,LTsv_keyboard_guideX,LTsv_keyboard_guideK,LTsv_keyboard_guideKN,LTsv_keyboard_guideKX
-
-def LTsv_keyboard_iroha_type():
-    return LTsv_keyboard_irohatype,LTsv_keyboard_alphatype,LTsv_keyboard_dictype,LTsv_keyboard_tofu
-
-def LTsv_keyboard_map(kanmapN=None,kanmapX=None,dicinput=None):
-    global LTsv_keyboard_kanmapN,LTsv_keyboard_kanmapX,LTsv_keyboard_dicinput
-    LTsv_keyboard_kanmapN=LTsv_keyboard_kanmapN     if kanmapN  == None else kanmapN
-    LTsv_keyboard_kanmapX=LTsv_keyboard_kanmapX     if kanmapX  == None else kanmapX
-    LTsv_keyboard_dicinput=LTsv_keyboard_dicinput   if dicinput == None else dicinput
-    return LTsv_keyboard_kanmapN,LTsv_keyboard_kanmapX,LTsv_keyboard_dicinput
-
-def LTsv_keyboard_dic(kandic=None):
-    global LTsv_keyboard_kandic
-    LTsv_keyboard_kandic=LTsv_keyboard_kandic   if kandic == None else kandic
-    return LTsv_keyboard_kandic
-
-def LTsv_keyboard_NXK(cursorMS=None,cursorIR=None,cursorAF=None,cursorOLD=None,cursorDIC=None,cursorNX=None,cursorK=None,cursorLCR=None):
-    global LTsv_keyboard_cursorMS,LTsv_keyboard_cursorIR,LTsv_keyboard_cursorAF,LTsv_keyboard_cursorOLD,LTsv_keyboard_cursorDIC,LTsv_keyboard_cursorNX,LTsv_keyboard_cursorK,LTsv_keyboard_cursorLCR
-    LTsv_keyboard_cursorMS=LTsv_keyboard_cursorMS   if cursorMS  == None else cursorMS
-    LTsv_keyboard_cursorIR=LTsv_keyboard_cursorIR   if cursorIR  == None else cursorIR
-    LTsv_keyboard_cursorAF=LTsv_keyboard_cursorAF   if cursorAF  == None else cursorAF
-    LTsv_keyboard_cursorOLD=LTsv_keyboard_cursorOLD if cursorOLD == None else cursorOLD
-    LTsv_keyboard_cursorDIC=LTsv_keyboard_cursorDIC if cursorDIC == None else cursorDIC
-    LTsv_keyboard_cursorNX=LTsv_keyboard_cursorNX   if cursorNX  == None else cursorNX
-    LTsv_keyboard_cursorK=LTsv_keyboard_cursorK     if cursorK   == None else cursorK
-    LTsv_keyboard_cursorLCR=LTsv_keyboard_cursorLCR if cursorLCR == None else cursorLCR
-    return LTsv_keyboard_cursorMS,LTsv_keyboard_cursorIR,LTsv_keyboard_cursorAF,LTsv_keyboard_cursorOLD,LTsv_keyboard_cursorDIC,LTsv_keyboard_cursorNX,LTsv_keyboard_cursorK,LTsv_keyboard_cursorLCR
-
-def LTsv_keyboard_findNX():
-    global LTsv_keyboard_findN,LTsv_keyboard_findX,LTsv_keyboard_findNr,LTsv_keyboard_findXr
-    return LTsv_keyboard_findN,LTsv_keyboard_findX,LTsv_keyboard_findNr,LTsv_keyboard_findXr
 
 
 if __name__=="__main__":
