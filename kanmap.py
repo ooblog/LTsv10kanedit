e4516e18b4704c8c4d47b4f41e5a44e55922ea80
ooblog <ooblog@gmail.com>
2016-10-09 02:36:27 +0900

    「kanmap.py」で「LTsv/kanmap.tsv」だけでなく「LTsv/kanchar.tsv」のフォントグリフも表示できるよう調整。

--- kanmap.py
+++ kanmap.py
@@ -3,6 +3,7 @@
 from __future__ import division,print_function,absolute_import,unicode_literals
 import sys
 import os
+import math
 os.chdir(sys.path[0])
 sys.path.append("LTsv")
 from LTsv_printf  import *
@@ -16,34 +17,16 @@ from LTsv_glyph  import *
 
 
 LTsv_irohaalpha_count=0
-def LTsv_kanmap_drawZero(window_objvoid=None,window_objptr=None):
+def LTsv_kanmap_drawKBDZero():
     global LTsv_irohaalpha_count
     LTsv_widget_settext(kanmap_window,"kanmap:pickling kanmap.tsv")
     LTsv_draw_selcanvas(kanmap_canvas); LTsv_draw_delete(); LTsv_draw_queue();
+    LTsv_glyph_kbddelete(kanmap_canvas)
+    LTsv_glyph_kbddraw(kanmap_canvas,kanmap_kbdX,kanmap_kbdY); LTsv_draw_queue();
     LTsv_irohaalpha_count=0
-    LTsv_window_after(kanmap_window,event_b=LTsv_kanmap_drawCount,event_i="LTsv_kanmap_drawCount",event_w=1)
-
-def LTsv_kanmap_drawCount(window_objvoid=None,window_objptr=None):
-    global LTsv_irohaalpha_count
-    LTsv_kanmap_drawKBD(LTsv_irohaalpha_count); LTsv_draw_queue();
-    LTsv_irohaalpha_count+=1
-    if LTsv_irohaalpha_count < len(LTsv_global_irohaalpha()):
-        LTsv_window_after(kanmap_window,event_b=LTsv_kanmap_drawCount,event_i="LTsv_kanmap_drawCount",event_w=10)
-    else:
-        LTsv_widget_settext(kanmap_window,"kanmap")
+    LTsv_window_after(kanmap_window,event_b=LTsv_kanmap_drawKBDCount,event_i="LTsv_kanmap_drawKBDCount",event_w=10)
 
 def LTsv_kanmap_drawKBD(irohaalpha_count):
-#    LTsv_draw_selcanvas(kanmap_canvas); LTsv_draw_color("black"); LTsv_draw_bgcolor("white")
-#    kbd_x,kbd_y=irohaalpha_count%12*LTsv_glyph_kbdW,irohaalpha_count//12*kanmap_kbdNH
-#    LTsv_glyph_kbdchars[0:LTsv_glyph_irohamax]=LTsv_glyph_kanmapN[LTsv_global_irohaalpha()[irohaalpha_count]][0:LTsv_glyph_irohamax]; LTsv_glyph_kbdchars[LTsv_glyph_irohamax]=LTsv_global_irohaalphaN()[irohaalpha_count]
-#    for kbd_xy in range(LTsv_glyph_None):
-#        LTsv_draw_glyphskbd(draw_t=LTsv_glyph_kbdchars[kbd_xy],draw_x=kbd_x+LTsv_glyph_fontX[kbd_xy],draw_y=kbd_y+LTsv_glyph_fontY[kbd_xy],draw_f=LTsv_glyph_fontG[kbd_xy],draw_g=LTsv_glyph_kbdtype[kbd_xy])
-#    kbd_y+=kanmap_NH
-#    if irohaalpha_count > len(LTsv_global_irohaalpha())-4:
-#        kbd_x,kbd_y=kbd_x+LTsv_glyph_kbdW*9,kbd_y-kanmap_kbdNH*7
-#    LTsv_glyph_kbdchars[0:LTsv_glyph_irohamax]=LTsv_glyph_kanmapX[LTsv_global_irohaalpha()[irohaalpha_count]][0:LTsv_glyph_irohamax]; LTsv_glyph_kbdchars[LTsv_glyph_irohamax]=LTsv_global_irohaalphaX()[irohaalpha_count]
-#    for kbd_xy in range(LTsv_glyph_None):
-#        LTsv_draw_glyphskbd(draw_t=LTsv_glyph_kbdchars[kbd_xy],draw_x=kbd_x+LTsv_glyph_fontX[kbd_xy],draw_y=kbd_y+LTsv_glyph_fontY[kbd_xy],draw_f=LTsv_glyph_fontG[kbd_xy],draw_g=LTsv_glyph_kbdtype[kbd_xy])
     LTsv_draw_selcanvas(kanmap_canvas); LTsv_draw_color("black"); LTsv_draw_bgcolor("white")
     kbd_x,kbd_y=LTsv_glyph_kbdNX[irohaalpha_count],LTsv_glyph_kbdNY[irohaalpha_count]
     LTsv_glyph_kbdchars[0:LTsv_glyph_irohamax]=LTsv_glyph_kanmapN[LTsv_global_irohaalpha()[irohaalpha_count]][0:LTsv_glyph_irohamax]; LTsv_glyph_kbdchars[LTsv_glyph_irohamax]=LTsv_global_irohaalphaN()[irohaalpha_count]
@@ -54,8 +37,47 @@ def LTsv_kanmap_drawKBD(irohaalpha_count):
     for kbd_xy in range(LTsv_glyph_None):
         LTsv_draw_glyphskbd(draw_t=LTsv_glyph_kbdchars[kbd_xy],draw_x=kbd_x+LTsv_glyph_fontX[kbd_xy],draw_y=kbd_y+LTsv_glyph_fontY[kbd_xy],draw_f=LTsv_glyph_fontG[kbd_xy],draw_g=LTsv_glyph_kbdtype[kbd_xy])
 
+def LTsv_kanmap_drawKBDCount(window_objvoid=None,window_objptr=None):
+    global LTsv_irohaalpha_count
+    LTsv_kanmap_drawKBD(LTsv_irohaalpha_count); LTsv_draw_queue();
+    LTsv_irohaalpha_count+=1
+    if LTsv_irohaalpha_count < len(LTsv_global_irohaalpha()):
+        LTsv_window_after(kanmap_window,event_b=LTsv_kanmap_drawKBDCount,event_i="LTsv_kanmap_drawKBDCount",event_w=10)
+    else:
+        LTsv_window_after(kanmap_window,event_b=LTsv_kanmap_drawDICZero,event_i="LTsv_kanmap_drawDICZero",event_w=10)
 
+LTsv_kansplits_count=0
+def LTsv_kanmap_drawDICZero(window_objvoid=None,window_objptr=None):
+    LTsv_widget_settext(kanmap_window,"kanmap:pickling LTsv/kanchar.tsv")
+    LTsv_draw_selcanvas(kanmap_canvas); LTsv_draw_color("black"); LTsv_draw_bgcolor("black")
+    LTsv_draw_polygonfill(LTsv_glyph_kbdF//2,LTsv_glyph_dicY-LTsv_glyph_kbdF//2,kanmap_canvasW-LTsv_glyph_kbdF//2,LTsv_glyph_dicY-LTsv_glyph_kbdF//2,kanmap_canvasW-LTsv_glyph_kbdF//2,LTsv_glyph_dicY-LTsv_glyph_kbdF//2-1,LTsv_glyph_kbdF//2,LTsv_glyph_dicY-LTsv_glyph_kbdF//2-1)
+    LTsv_draw_queue()
+    LTsv_window_after(kanmap_window,event_b=LTsv_kanmap_drawDICCount,event_i="LTsv_kanmap_drawDICCount",event_w=10)
 
+def LTsv_kanmap_drawDIC(kansplits_count):
+    LTsv_draw_selcanvas(kanmap_canvas); LTsv_draw_color("black"); LTsv_draw_bgcolor("white")
+    dicY=LTsv_glyph_dicY+LTsv_glyph_kbdF*kansplits_count//kanmap_kansplits_linelen
+    for kbd_xy in range(min(kanmap_kansplits_linelen,len(LTsv_kansplits)-kansplits_count)):
+        LTsv_draw_glyphskbd(draw_t=LTsv_kansplits[kansplits_count+kbd_xy][0:1],draw_x=kbd_xy*LTsv_glyph_kbdF,draw_y=dicY)
+
+def LTsv_kanmap_drawDICCount(window_objvoid=None,window_objptr=None):
+    global LTsv_kansplits_count
+    LTsv_kanmap_drawDIC(LTsv_kansplits_count); LTsv_draw_queue();
+    LTsv_kansplits_count+=kanmap_kansplits_linelen
+    if LTsv_kansplits_count < len(LTsv_kansplits):
+        LTsv_window_after(kanmap_window,event_b=LTsv_kanmap_drawDICCount,event_i="LTsv_kanmap_drawDICCount",event_w=10)
+    else:
+        LTsv_widget_settext(kanmap_window,"kanmap")
+
+def LTsv_kanmap_mousepress(window_objvoid=None,window_objptr=None):
+    if LTsv_glyph_mousepress(kanmap_canvas,kanmap_kbdX,kanmap_kbdY) == LTsv_global_kbdcursorNone():
+        pass
+
+def LTsv_kanmap_mousemotion(window_objvoid=None,window_objptr=None):
+    LTsv_glyph_mousemotion(kanmap_canvas,kanmap_kbdX,kanmap_kbdY)
+
+def LTsv_kanmap_mouserelease(window_objvoid=None,window_objptr=None):
+    LTsv_glyph_mouserelease(kanmap_canvas,kanmap_kbdX,kanmap_kbdY)
 
 def kanmap_configsave_exit(window_objvoid=None,window_objptr=None):
     LTsv_glyph_picklesave()
@@ -79,20 +101,27 @@ if len(LTsv_GUI) > 0:
     LTsv_glyph_fontX[LTsv_glyph_irohamax],LTsv_glyph_fontY[LTsv_glyph_irohamax],LTsv_glyph_fontG[LTsv_glyph_irohamax]=LTsv_glyph_kbdG=1+LTsv_glyph_kbdF*0,1,LTsv_glyph_kbdG*2
     LTsv_glyph_kbdtype=tuple(["活" if t < LTsv_glyph_irohamax else "漫" for t in range(LTsv_glyph_None)])
     kanmap_kbdNH=LTsv_glyph_kbdH+LTsv_glyph_kbdF*2
-    kanmap_NH=kanmap_kbdNH*(4+3)
+    kanmap_NH=kanmap_kbdNH*(4+2)
     LTsv_glyph_kbdNX,LTsv_glyph_kbdNY=[0]*len(LTsv_global_irohaalpha()),[0]*len(LTsv_global_irohaalpha())
     LTsv_glyph_kbdXX,LTsv_glyph_kbdXY=[0]*len(LTsv_global_irohaalpha()),[0]*len(LTsv_global_irohaalpha())
     for map_xy in range(len(LTsv_global_irohaalpha())):
-        LTsv_glyph_kbdNX[map_xy],LTsv_glyph_kbdNY[map_xy]=map_xy%12*LTsv_glyph_kbdW,map_xy//12*kanmap_kbdNH
-        LTsv_glyph_kbdXX[map_xy],LTsv_glyph_kbdXY[map_xy]=map_xy%12*LTsv_glyph_kbdW,map_xy//12*kanmap_kbdNH+kanmap_NH
-        if map_xy < len(LTsv_global_irohaalpha())-4: continue;
-        LTsv_glyph_kbdXX[map_xy],LTsv_glyph_kbdXY[map_xy]=LTsv_glyph_kbdXX[map_xy]+LTsv_glyph_kbdW*9,LTsv_glyph_kbdXY[map_xy]-kanmap_kbdNH*7
-    kanmap_canvasW,kanmap_canvasH=LTsv_glyph_kbdW*12,kanmap_NH*2-kanmap_kbdNH*1
+        if map_xy < len(LTsv_global_irohaalpha())-3:
+            LTsv_glyph_kbdNX[map_xy],LTsv_glyph_kbdNY[map_xy]=map_xy%12*LTsv_glyph_kbdW,map_xy//12*kanmap_kbdNH
+            LTsv_glyph_kbdXX[map_xy],LTsv_glyph_kbdXY[map_xy]=map_xy%12*LTsv_glyph_kbdW,map_xy//12*kanmap_kbdNH+kanmap_NH
+        else:
+            LTsv_glyph_kbdNX[map_xy],LTsv_glyph_kbdNY[map_xy]=LTsv_glyph_kbdW*12,(map_xy%12+0)*kanmap_kbdNH
+            LTsv_glyph_kbdXX[map_xy],LTsv_glyph_kbdXY[map_xy]=LTsv_glyph_kbdW*12,(map_xy%12+3)*kanmap_kbdNH+kanmap_NH
+    LTsv_glyph_dicY=kanmap_NH*2+LTsv_glyph_kbdF
+    kanmap_canvasW,kanmap_canvasH=LTsv_glyph_kbdF*int(math.ceil(1000/LTsv_glyph_kbdF)),LTsv_glyph_dicY
+    kanmap_kansplits_linelen=kanmap_canvasW//LTsv_glyph_kbdF
+    LTsv_glyph_kandic=LTsv_global_kandic(); LTsv_kansplits=LTsv_glyph_kandic.rstrip('\n').split('\n')
+    LTsv_kansplits_len=len(LTsv_kansplits)//kanmap_kansplits_linelen
+    kanmap_canvasH=kanmap_canvasH+LTsv_kansplits_len*LTsv_glyph_kbdF
+    kanmap_kbdX,kanmap_kbdY=kanmap_canvasW-LTsv_global_glyphkbdW()-LTsv_glyph_kbdF,kanmap_NH-kanmap_kbdNH//2
     kanmapW,kanmapH=kanmap_canvasW,kanmap_canvasH
     kanmap_window=LTsv_window_new(widget_t="kanmap",event_b=kanmap_configsave_exit,widget_w=kanmapW,widget_h=kanmapH,event_k=None,event_y=None)
     kanmap_canvas=LTsv_canvas_new(kanmap_window,widget_x=0,widget_y=0,widget_w=kanmap_canvasW,widget_h=kanmap_canvasH,
-#     event_p=kanmap_mousepress,event_m=kanmap_mousemotion,event_r=kanmap_mouserelease,event_e=kanmap_mouseenter,event_l=kanmap_mouseleave,event_w=50)
-     event_p=None,event_m=None,event_r=None,event_e=None,event_l=None,event_w=50)
+     event_p=LTsv_kanmap_mousepress,event_m=LTsv_kanmap_mousemotion,event_r=LTsv_kanmap_mouserelease,event_e=None,event_l=None,event_w=50)
     LTsv_widget_showhide(kanmap_window,True)
     LTsv_draw_selcanvas,LTsv_draw_delete,LTsv_draw_queue,LTsv_draw_picture=LTsv_draw_selcanvas_shell(LTsv_GUI),LTsv_draw_delete_shell(LTsv_GUI),LTsv_draw_queue_shell(LTsv_GUI),LTsv_draw_picture_shell(LTsv_GUI)
     LTsv_draw_color,LTsv_draw_bgcolor,LTsv_draw_font,LTsv_draw_text=LTsv_draw_color_shell(LTsv_GUI),LTsv_draw_bgcolor_shell(LTsv_GUI),LTsv_draw_font_shell(LTsv_GUI),LTsv_draw_text_shell(LTsv_GUI)
@@ -101,7 +130,7 @@ if len(LTsv_GUI) > 0:
     LTsv_draw_circles,LTsv_draw_circlesfill=LTsv_draw_circles_shell(LTsv_GUI),LTsv_draw_circlesfill_shell(LTsv_GUI)
     LTsv_draw_points=LTsv_draw_points_shell(LTsv_GUI)
     LTsv_draw_arc,LTsv_draw_arcfill=LTsv_draw_arc_shell(LTsv_GUI),LTsv_draw_arcfill_shell(LTsv_GUI)
-    LTsv_kanmap_drawZero()
+    LTsv_kanmap_drawKBDZero()
     LTsv_window_main(kanmap_window)
 else:
     LTsv_libc_printf("GUIの設定に失敗しました。")
